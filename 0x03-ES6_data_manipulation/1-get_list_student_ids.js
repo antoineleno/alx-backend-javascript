5f5817739f61147b5ed64dc2e75515c78a3e75fa
antoineleno <johndoe@example.com>
2024-10-08 13:34:10 +0800

    ES6 data manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,7 +1,14 @@
- // More mapping
+/**
+ * Returns an array of student IDs from a list of student objects.
+ * If the input is not an array, it returns an empty array.
+ *
+ * @param {Array} students - The array of student objects.
+ * Each student object should contain an `id` field.
+ * @returns {Array} List of student IDs.
+ */
 export default function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-    return students.map(student => student.id);
+  if (!Array.isArray(students)) {
+    return [];
   }
+  return students.map((student) => student.id);
+}
